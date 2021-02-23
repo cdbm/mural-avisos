aa4c5e55072f4e2fa346627230083647c10e775b
cdbm <cdbm@cin.ufpe.br>
2021-02-23 20:42:54 -0300

    basic file strcture

---
+++ rest-avisos/src/main/java/com/cdbm/restavisos/RestAvisosApplication.java
@@ -0,0 +1,13 @@
+package com.cdbm.restavisos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAvisosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAvisosApplication.class, args);
+	}
+
+}
---
+++ rest-avisos/src/test/java/com/cdbm/restavisos/RestAvisosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdbm.restavisos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAvisosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
